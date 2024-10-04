cc3eca05f0063a5556a8521e8450d4f654af217b
Eyada <mohamed.ayada.ma@gmail.com>
2024-10-04 18:17:56 +0300

    feat(RecipeCard): handel style for recipe

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
+import { useLocation } from "react-router-dom"; 
 import Sidebar from "./components/sidebar";
 import HomePage from "./pages/HomePage";
-import { Route, Routes } from "react-router-dom";
 import FavoritesPage from "./pages/FavoritesPage";
+import NotFound from "./pages/NotFound";
+import { Route, Routes } from "react-router-dom";
+
 function App() {
+  const location = useLocation(); 
+
   return (
     <div className="flex">
-      <Sidebar />
+      {location.pathname !== "/404" && <Sidebar />}
+      
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/favorites" element={<FavoritesPage />} />
+        <Route path="/favorite" element={<FavoritesPage />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
   );
